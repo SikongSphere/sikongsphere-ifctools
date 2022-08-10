0dec0e373041beadd2882c2147b82e9d8a45385d
beecorn <bohong_wang@hotmail.com>
2022-08-10 10:09:39 +0000

    !2 spotless * 增加license-header * 增加license-header * spotless

--- src/main/java/org/sikongsphere/ifc/infra/spi/App.java
+++ src/main/java/org/sikongsphere/ifc/infra/spi/App.java
@@ -1,11 +1,20 @@
+/*
+ * Copyright 2022 SikongSphere
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+*/
 package org.sikongsphere.ifc.infra.spi;
-
-import org.sikongsphere.ifc.infra.spi.impl.HelloServiceImpl1;
-import sun.misc.Service;
 
 import java.lang.reflect.Constructor;
 import java.util.Iterator;
 import java.util.ServiceLoader;
+import org.sikongsphere.ifc.infra.spi.impl.HelloServiceImpl1;
+import sun.misc.Service;
 
 public class App {
     public static void main(String[] args) throws NoSuchMethodException {
--- src/main/java/org/sikongsphere/ifc/infra/spi/impl/HelloServiceImpl1.java
+++ src/main/java/org/sikongsphere/ifc/infra/spi/impl/HelloServiceImpl1.java
@@ -1,16 +1,22 @@
+/*
+ * Copyright 2022 SikongSphere
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+*/
 package org.sikongsphere.ifc.infra.spi.impl;
 
 import org.sikongsphere.ifc.infra.spi.HelloService;
 
 public class HelloServiceImpl1 implements HelloService {
 
-    public HelloServiceImpl1() {
+    public HelloServiceImpl1() {}
 
-    }
-
-    public HelloServiceImpl1(String str1) {
-
-    }
+    public HelloServiceImpl1(String str1) {}
 
     @Override
     public void sayHello() {
