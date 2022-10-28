e1d09db19340608e82e46e28b3615b64a5ea489c
sunyiwei <736178452@qq.com>
2022-10-28 17:16:30 +0800

    修改

--- src/main/java/org/sikongsphere/ifc/model/shared/sharedbldelements/entity/IfcMember.java
+++ src/main/java/org/sikongsphere/ifc/model/shared/sharedbldelements/entity/IfcMember.java
@@ -8,34 +8,49 @@
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * General Public License for more details.
 */
-package org.sikongsphere.ifc.model.core.kernel.entity;
+package org.sikongsphere.ifc.model.shared.sharedbldelements.entity;
 
 import org.sikongsphere.ifc.common.annotation.IfcClass;
 import org.sikongsphere.ifc.common.annotation.IfcParserConstructor;
 import org.sikongsphere.ifc.common.enumeration.IfcLayer;
 import org.sikongsphere.ifc.common.enumeration.IfcType;
+import org.sikongsphere.ifc.model.core.productextension.entities.IfcBuildingElement;
+import org.sikongsphere.ifc.model.resource.geometricconstraint.entity.IfcObjectPlacement;
+import org.sikongsphere.ifc.model.resource.measure.definedtype.IfcIdentifier;
 import org.sikongsphere.ifc.model.resource.measure.definedtype.IfcLabel;
 import org.sikongsphere.ifc.model.resource.measure.definedtype.IfcText;
+import org.sikongsphere.ifc.model.resource.representation.entity.IfcProductRepresentation;
 import org.sikongsphere.ifc.model.resource.utility.definedtype.IfcGloballyUniqueId;
 import org.sikongsphere.ifc.model.resource.utility.entity.IfcOwnerHistory;
 
 /**
- * IfcRelConnects
- *
- * @author zaiyuan
- * @date 2022/9/1 08:15
+ * @author Yiwei
+ * @date 2022/10/28
  */
-@IfcClass(type = IfcType.ENTITY, layer = IfcLayer.CORE)
-public abstract class IfcRelConnects extends IfcRelationship {
-    public IfcRelConnects() {}
+@IfcClass(type = IfcType.ENTITY, layer = IfcLayer.SHARED)
+public class IfcMember extends IfcBuildingElement {
+    public IfcMember() {}
 
     @IfcParserConstructor
-    public IfcRelConnects(
+    public IfcMember(
         IfcGloballyUniqueId globalId,
         IfcOwnerHistory ownerHistory,
         IfcLabel name,
-        IfcText description
+        IfcText description,
+        IfcLabel objectType,
+        IfcObjectPlacement objectPlacement,
+        IfcProductRepresentation representation,
+        IfcIdentifier tag
     ) {
-        super(globalId, ownerHistory, name, description);
+        super(
+            globalId,
+            ownerHistory,
+            name,
+            description,
+            objectType,
+            objectPlacement,
+            representation,
+            tag
+        );
     }
 }
