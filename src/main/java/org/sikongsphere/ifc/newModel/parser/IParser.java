73102dd8d37963a81f71b14cec193f5377c44cdc
zaiyuan49 <zhugeliang0511>
2022-12-04 17:27:47 +0800

    链接功能完成

---
+++ src/main/java/org/sikongsphere/ifc/newModel/parser/IParser.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2022 SikongSphere
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+*/
+package org.sikongsphere.ifc.newModel.parser;
+
+import org.sikongsphere.ifc.newModel.Model;
+
+import java.io.IOException;
+
+/**
+ * Interface for different Parser
+ * @author zaiyuan
+ * @date 2022/12/04 15:27
+ */
+public interface IParser {
+
+    Model parseFile(String filePath) throws IOException;
+}
