d9b2143ce44f7b20debaad962eb0e19bb6a3302f
艺玮 <11560403+sun-iwei@user.noreply.gitee.com>
2022-12-10 13:12:43 +0000

    !156 controlextention * controlextention初步

---
+++ src/main/java/org/sikongsphere/ifc/model/schema/extension/control/entity/IfcRelAssociatesApproval.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2022 SikongSphere
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+*/
+package org.sikongsphere.ifc.model.schema.extension.control.entity;
+
+import org.sikongsphere.ifc.common.annotation.IfcClass;
+import org.sikongsphere.ifc.common.annotation.IfcParserConstructor;
+import org.sikongsphere.ifc.common.enumeration.IfcLayer;
+import org.sikongsphere.ifc.common.enumeration.IfcType;
+import org.sikongsphere.ifc.model.datatype.SET;
+import org.sikongsphere.ifc.model.schema.kernel.entity.IfcControl;
+import org.sikongsphere.ifc.model.schema.kernel.entity.IfcRelAssignsToControl;
+import org.sikongsphere.ifc.model.schema.kernel.entity.IfcRelAssociates;
+import org.sikongsphere.ifc.model.schema.resource.measure.definedType.IfcLabel;
+import org.sikongsphere.ifc.model.schema.resource.measure.definedType.IfcText;
+import org.sikongsphere.ifc.model.schema.resource.utility.definedtype.IfcGloballyUniqueId;
+import org.sikongsphere.ifc.model.schema.resource.utility.entity.IfcOwnerHistory;
+
+/**
+ * @author zaiyuan
+ * @date 2022/12/10 23:15
+ */
+@IfcClass(layer = IfcLayer.CORE, type = IfcType.ENTITY)
+public class IfcRelAssociatesApproval extends IfcRelAssociates {
+    // private IfcApproval relatingApproval;
+    // todo
+}
