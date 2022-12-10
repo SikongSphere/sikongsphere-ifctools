9edb83b1beb814a1fc1398edb3cad3a40d403197
艺玮 <11560403+sun-iwei@user.noreply.gitee.com>
2022-12-10 13:57:07 +0000

    !157 时间序列 * timeseries初步 * controlextention初步

--- src/main/java/org/sikongsphere/ifc/model/schema/extension/control/entity/IfcTimeSeriesSchedule.java
+++ src/main/java/org/sikongsphere/ifc/model/schema/extension/control/entity/IfcTimeSeriesSchedule.java
@@ -12,15 +12,18 @@ package org.sikongsphere.ifc.model.schema.extension.control.entity;
 
 import org.sikongsphere.ifc.common.annotation.IfcClass;
 import org.sikongsphere.ifc.common.annotation.IfcOptionField;
+import org.sikongsphere.ifc.common.annotation.IfcParserConstructor;
 import org.sikongsphere.ifc.common.enumeration.IfcLayer;
 import org.sikongsphere.ifc.common.enumeration.IfcType;
 import org.sikongsphere.ifc.model.datatype.SET;
+import org.sikongsphere.ifc.model.schema.extension.control.enumeration.IfcTimeSeriesScheduleTypeEnum;
 import org.sikongsphere.ifc.model.schema.kernel.entity.IfcControl;
 import org.sikongsphere.ifc.model.schema.kernel.entity.IfcRelAssignsToControl;
 import org.sikongsphere.ifc.model.schema.kernel.entity.IfcRelAssociates;
 import org.sikongsphere.ifc.model.schema.resource.datetime.selectType.IfcDateTimeSelect;
 import org.sikongsphere.ifc.model.schema.resource.measure.definedType.IfcLabel;
 import org.sikongsphere.ifc.model.schema.resource.measure.definedType.IfcText;
+import org.sikongsphere.ifc.model.schema.resource.timeseries.entity.IfcTimeSeries;
 import org.sikongsphere.ifc.model.schema.resource.utility.definedtype.IfcGloballyUniqueId;
 import org.sikongsphere.ifc.model.schema.resource.utility.entity.IfcOwnerHistory;
 
@@ -32,8 +35,10 @@ import org.sikongsphere.ifc.model.schema.resource.utility.entity.IfcOwnerHistory
 public class IfcTimeSeriesSchedule extends IfcControl {
     @IfcOptionField
     private IfcDateTimeSelect applicableDates;
-    // todo
+    private IfcTimeSeriesScheduleTypeEnum timeSeriesScheduleType;
+    private IfcTimeSeries timeSeries;
 
+    @IfcParserConstructor
     public IfcTimeSeriesSchedule(
         IfcGloballyUniqueId globalId,
         IfcOwnerHistory ownerHistory,
@@ -41,9 +46,37 @@ public class IfcTimeSeriesSchedule extends IfcControl {
         IfcText description,
         IfcLabel objectType,
         SET<IfcRelAssignsToControl> controls,
-        IfcDateTimeSelect applicableDates
+        IfcDateTimeSelect applicableDates,
+        IfcTimeSeriesScheduleTypeEnum timeSeriesScheduleType,
+        IfcTimeSeries timeSeries
     ) {
         super(globalId, ownerHistory, name, description, objectType, controls);
         this.applicableDates = applicableDates;
+        this.timeSeriesScheduleType = timeSeriesScheduleType;
+        this.timeSeries = timeSeries;
+    }
+
+    public IfcDateTimeSelect getApplicableDates() {
+        return applicableDates;
+    }
+
+    public void setApplicableDates(IfcDateTimeSelect applicableDates) {
+        this.applicableDates = applicableDates;
+    }
+
+    public IfcTimeSeriesScheduleTypeEnum getTimeSeriesScheduleType() {
+        return timeSeriesScheduleType;
+    }
+
+    public void setTimeSeriesScheduleType(IfcTimeSeriesScheduleTypeEnum timeSeriesScheduleType) {
+        this.timeSeriesScheduleType = timeSeriesScheduleType;
+    }
+
+    public IfcTimeSeries getTimeSeries() {
+        return timeSeries;
+    }
+
+    public void setTimeSeries(IfcTimeSeries timeSeries) {
+        this.timeSeries = timeSeries;
     }
 }
---
+++ src/main/java/org/sikongsphere/ifc/model/schema/resource/timeseries/entity/IfcTimeSeries.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2022 SikongSphere
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+*/
+package org.sikongsphere.ifc.model.schema.resource.timeseries.entity;
+
+import org.sikongsphere.ifc.common.annotation.IfcClass;
+import org.sikongsphere.ifc.common.annotation.IfcInverseParameter;
+import org.sikongsphere.ifc.common.annotation.IfcOptionField;
+import org.sikongsphere.ifc.common.annotation.IfcParserConstructor;
+import org.sikongsphere.ifc.common.enumeration.IfcLayer;
+import org.sikongsphere.ifc.common.enumeration.IfcType;
+import org.sikongsphere.ifc.model.IfcAbstractClass;
+import org.sikongsphere.ifc.model.datatype.LIST;
+import org.sikongsphere.ifc.model.datatype.SET;
+import org.sikongsphere.ifc.model.schema.resource.datetime.selectType.IfcDateTimeSelect;
+import org.sikongsphere.ifc.model.schema.resource.measure.definedType.IfcLabel;
+import org.sikongsphere.ifc.model.schema.resource.measure.definedType.IfcText;
+import org.sikongsphere.ifc.model.schema.resource.measure.selectTypes.IfcUnit;
+import org.sikongsphere.ifc.model.schema.resource.measure.selectTypes.IfcValue;
+import org.sikongsphere.ifc.model.schema.resource.timeseries.enumeration.IfcDataOriginEnum;
+import org.sikongsphere.ifc.model.schema.resource.timeseries.enumeration.IfcTimeSeriesDataTypeEnum;
+
+/**
+ * @author yiwei
+ * @date 2022/11/01 23:09
+ */
+@IfcClass(type = IfcType.ENTITY, layer = IfcLayer.RESOURCE)
+public class IfcTimeSeries extends IfcAbstractClass {
+    private IfcLabel name;
+    @IfcOptionField
+    private IfcText description;
+    private IfcDateTimeSelect startTime;
+    private IfcDateTimeSelect endTime;
+    private IfcTimeSeriesDataTypeEnum timeSeriesDataType;
+    private IfcDataOriginEnum dataOrigin;
+    @IfcOptionField
+    private IfcLabel userDefinedDataOrigin;
+    @IfcOptionField
+    private IfcUnit unit;
+    @IfcInverseParameter
+    private SET<IfcTimeSeriesReferenceRelationship> documentedBy;
+
+    @IfcParserConstructor
+    public IfcTimeSeries(
+        IfcLabel name,
+        IfcText description,
+        IfcDateTimeSelect startTime,
+        IfcDateTimeSelect endTime,
+        IfcTimeSeriesDataTypeEnum timeSeriesDataType,
+        IfcDataOriginEnum dataOrigin,
+        IfcLabel userDefinedDataOrigin,
+        IfcUnit unit
+    ) {
+        this.name = name;
+        this.description = description;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.timeSeriesDataType = timeSeriesDataType;
+        this.dataOrigin = dataOrigin;
+        this.userDefinedDataOrigin = userDefinedDataOrigin;
+        this.unit = unit;
+    }
+
+    public IfcLabel getName() {
+        return name;
+    }
+
+    public void setName(IfcLabel name) {
+        this.name = name;
+    }
+
+    public IfcText getDescription() {
+        return description;
+    }
+
+    public void setDescription(IfcText description) {
+        this.description = description;
+    }
+
+    public IfcDateTimeSelect getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(IfcDateTimeSelect startTime) {
+        this.startTime = startTime;
+    }
+
+    public IfcDateTimeSelect getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(IfcDateTimeSelect endTime) {
+        this.endTime = endTime;
+    }
+
+    public IfcTimeSeriesDataTypeEnum getTimeSeriesDataType() {
+        return timeSeriesDataType;
+    }
+
+    public void setTimeSeriesDataType(IfcTimeSeriesDataTypeEnum timeSeriesDataType) {
+        this.timeSeriesDataType = timeSeriesDataType;
+    }
+
+    public IfcDataOriginEnum getDataOrigin() {
+        return dataOrigin;
+    }
+
+    public void setDataOrigin(IfcDataOriginEnum dataOrigin) {
+        this.dataOrigin = dataOrigin;
+    }
+
+    public IfcLabel getUserDefinedDataOrigin() {
+        return userDefinedDataOrigin;
+    }
+
+    public void setUserDefinedDataOrigin(IfcLabel userDefinedDataOrigin) {
+        this.userDefinedDataOrigin = userDefinedDataOrigin;
+    }
+
+    public IfcUnit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(IfcUnit unit) {
+        this.unit = unit;
+    }
+
+    public SET<IfcTimeSeriesReferenceRelationship> getDocumentedBy() {
+        return documentedBy;
+    }
+
+    public void setDocumentedBy(SET<IfcTimeSeriesReferenceRelationship> documentedBy) {
+        this.documentedBy = documentedBy;
+    }
+}
