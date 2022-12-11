36d8dc8d298d9ea797bb4921f3590e06cfa52e3c
TUAN <785313020@qq>
2022-12-11 22:02:41 +0800

    Presentation Dimension

---
+++ src/main/java/org/sikongsphere/ifc/model/schema/resource/presentationdefinition/entity/IfcPreDefinedSymbol.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2022 SikongSphere
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+*/
+package org.sikongsphere.ifc.model.schema.resource.presentationdefinition.entity;
+
+import org.sikongsphere.ifc.common.annotation.IfcClass;
+import org.sikongsphere.ifc.common.annotation.IfcParserConstructor;
+import org.sikongsphere.ifc.common.enumeration.IfcLayer;
+import org.sikongsphere.ifc.common.enumeration.IfcType;
+import org.sikongsphere.ifc.model.schema.resource.presentation.entity.IfcPreDefinedItem;
+import org.sikongsphere.ifc.model.schema.resource.presentationorganization.entity.IfcStyledItem;
+
+/**
+ *
+ *
+ * @author GaoSu
+ * @date 2022/12/11 21:49
+ */
+@IfcClass(layer = IfcLayer.RESOURCE, type = IfcType.ENTITY)
+public class IfcPreDefinedSymbol extends IfcPreDefinedItem {
+    @IfcParserConstructor
+    public IfcPreDefinedSymbol() {}
+}
