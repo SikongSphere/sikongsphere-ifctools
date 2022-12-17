682147ea62f95ec6026dc1f93267c1d31f760f0e
zaiyuan49 <zhugeliang0511>
2022-12-17 23:31:27 +0800

    add control

--- src/main/java/org/sikongsphere/ifc/model/schema/extension/control/entity/IfcRelAssociatesConstraint.java
+++ src/main/java/org/sikongsphere/ifc/model/schema/extension/control/entity/IfcRelAssociatesConstraint.java
@@ -11,10 +11,17 @@
 package org.sikongsphere.ifc.model.schema.extension.control.entity;
 
 import org.sikongsphere.ifc.common.annotation.IfcClass;
+import org.sikongsphere.ifc.common.annotation.IfcParserConstructor;
 import org.sikongsphere.ifc.common.enumeration.IfcLayer;
 import org.sikongsphere.ifc.common.enumeration.IfcType;
+import org.sikongsphere.ifc.model.datatype.SET;
 import org.sikongsphere.ifc.model.schema.kernel.entity.IfcRelAssociates;
+import org.sikongsphere.ifc.model.schema.kernel.entity.IfcRoot;
+import org.sikongsphere.ifc.model.schema.resource.constraint.entity.IfcConstraint;
 import org.sikongsphere.ifc.model.schema.resource.measure.definedType.IfcLabel;
+import org.sikongsphere.ifc.model.schema.resource.measure.definedType.IfcText;
+import org.sikongsphere.ifc.model.schema.resource.utility.definedtype.IfcGloballyUniqueId;
+import org.sikongsphere.ifc.model.schema.resource.utility.entity.IfcOwnerHistory;
 
 /**
  * @author zaiyuan
@@ -23,6 +30,36 @@ import org.sikongsphere.ifc.model.schema.resource.measure.definedType.IfcLabel;
 @IfcClass(layer = IfcLayer.CORE, type = IfcType.ENTITY)
 public class IfcRelAssociatesConstraint extends IfcRelAssociates {
     private IfcLabel intent;
-    // private IfcConstraint relatingConstraint
-    // todo
+    private IfcConstraint relatingConstraint;
+
+    @IfcParserConstructor
+    public IfcRelAssociatesConstraint(
+        IfcGloballyUniqueId globalId,
+        IfcOwnerHistory ownerHistory,
+        IfcLabel name,
+        IfcText description,
+        SET<IfcRoot> relatedObjects,
+        IfcLabel intent,
+        IfcConstraint relatingConstraint
+    ) {
+        super(globalId, ownerHistory, name, description, relatedObjects);
+        this.intent = intent;
+        this.relatingConstraint = relatingConstraint;
+    }
+
+    public IfcLabel getIntent() {
+        return intent;
+    }
+
+    public void setIntent(IfcLabel intent) {
+        this.intent = intent;
+    }
+
+    public IfcConstraint getRelatingConstraint() {
+        return relatingConstraint;
+    }
+
+    public void setRelatingConstraint(IfcConstraint relatingConstraint) {
+        this.relatingConstraint = relatingConstraint;
+    }
 }
