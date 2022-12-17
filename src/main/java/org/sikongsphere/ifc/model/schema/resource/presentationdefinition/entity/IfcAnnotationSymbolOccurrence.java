7f82897c765081f7d8ff37c121b05aa67de317b1
zaiyuan49 <zhugeliang0511>
2022-12-17 20:09:02 +0800

    add presentation dimension

--- src/main/java/org/sikongsphere/ifc/model/schema/resource/presentationdefinition/entity/IfcAnnotationSymbolOccurrence.java
+++ src/main/java/org/sikongsphere/ifc/model/schema/resource/presentationdefinition/entity/IfcAnnotationSymbolOccurrence.java
@@ -13,6 +13,7 @@ package org.sikongsphere.ifc.model.schema.resource.presentationdefinition.entity
 import org.sikongsphere.ifc.common.annotation.IfcClass;
 import org.sikongsphere.ifc.common.enumeration.IfcLayer;
 import org.sikongsphere.ifc.common.enumeration.IfcType;
+import org.sikongsphere.ifc.model.schema.resource.presentationdimension.selecttype.IfcDraughtingCalloutElement;
 
 /**
  * An annotation symbol occurrence is a symbol with a style assignment.
@@ -21,4 +22,6 @@ import org.sikongsphere.ifc.common.enumeration.IfcType;
  * @date 2022/9/2 11:40
  */
 @IfcClass(layer = IfcLayer.RESOURCE, type = IfcType.ENTITY)
-public class IfcAnnotationSymbolOccurrence extends IfcAnnotationOccurrence {}
+public class IfcAnnotationSymbolOccurrence extends IfcAnnotationOccurrence
+    implements
+        IfcDraughtingCalloutElement {}
