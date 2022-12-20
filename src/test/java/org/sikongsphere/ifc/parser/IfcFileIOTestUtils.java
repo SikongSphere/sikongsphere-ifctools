eebe83f19c671c70a3bf031433f0ac463c8adf97
a-chaochao <liangc0427@sikongsphere.org>
2022-12-20 20:15:53 +0800

    将ifc文件迁移至hadoop，利用url的方式进行io测试

--- src/test/java/org/sikongsphere/ifc/parser/IfcFileIOTestUtils.java
+++ src/test/java/org/sikongsphere/ifc/parser/IfcFileIOTestUtils.java
@@ -11,24 +11,39 @@
 package org.sikongsphere.ifc.parser;
 
 import org.antlr.v4.runtime.CharStreams;
+import org.apache.commons.io.FileUtils;
 import org.sikongsphere.ifc.io.IfcFileReader;
 import org.sikongsphere.ifc.io.IfcFileWriter;
 import org.sikongsphere.ifc.model.fileelement.IfcFileModel;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
 public class IfcFileIOTestUtils {
 
+    public static final String BLANK_INPUT_PATH = "src/test/resources/temp.ifc";
     public static final String BLANK_OUTPUT_PATH = "src/test/resources/output.ifc";
 
     public static void ioTest(String input, String output) throws IOException {
         IfcFileModel model = IfcFileReader.readFile(input);
         IfcFileWriter.writeFile(model, output);
 
-        String ifcFileOne = CharStreams.fromFileName(input).toString().replaceAll("\\s*|\r", "");
+        String ifcFileOne = CharStreams.fromFileName(input)
+                .toString()
+                .replaceAll("\\s*|\r", "");
 
-        String ifcFileTwo = CharStreams.fromFileName(output).toString().replaceAll("\\s*|\r", "");
+        String ifcFileTwo = CharStreams.fromFileName(output)
+                .toString().
+                replaceAll("\\s*|\r", "");
 
         assert ifcFileOne.equals(ifcFileTwo);
+    }
+
+    public static void getIfcFile(String address) throws IOException {
+        URL url = new URL(address);
+        File file = new File(BLANK_INPUT_PATH);
+
+        FileUtils.copyURLToFile(url, file);
     }
 }
