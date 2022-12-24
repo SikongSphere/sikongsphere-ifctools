444205b04c86568976a185c46e4485d58b2db09f
ls1995516 <ls1995516@gmail.com>
2022-12-23 17:40:13 -0800

    Setup graph processor.

---
+++ src/main/java/org/sikongsphere/ifc/graph/processor/IFCPredictGraphProcessor.java
@@ -0,0 +1,19 @@
+package org.sikongsphere.ifc.graph.processor;
+
+import org.sikongsphere.ifc.graph.basic.IFCGraph;
+import org.sikongsphere.ifc.graph.basic.IFCGraphLayer;
+
+public class IFCPredictGraphProcessor implements IFCGraphProcessor{
+    public IFCPredictGraphProcessor() {
+    }
+
+    @Override
+    public IFCGraph Process(IFCGraph graph) {
+        return null;
+    }
+
+    @Override
+    public IFCGraphLayer ProcessLayer(IFCGraphLayer ifcGraphLayer) {
+        return null;
+    }
+}
---
+++ src/main/java/org/sikongsphere/ifc/graph/processor/IFCStandardGraphManager.java
@@ -0,0 +1,13 @@
+package org.sikongsphere.ifc.graph.processor;
+
+import org.sikongsphere.ifc.graph.basic.IFCGraph;
+
+public class IFCStandardGraphManager implements IFCGraphManager{
+    public IFCStandardGraphManager() {
+
+    }
+    @Override
+    public IFCGraph Process(IFCGraph graph, IFCGraphProcessor processor) {
+        return null;
+    }
+}
