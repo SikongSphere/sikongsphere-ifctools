36dcaf8f8d302ce0d8c5cda303c657ed6e9fa892
ls1995516 <ls1995516@gmail.com>
2023-02-11 20:46:14 -0800

    Add demo.

---
+++ src/test/java/org/sikongsphere/ifc/graph/GraphDemo.java
@@ -0,0 +1,19 @@
+package org.sikongsphere.ifc.graph;
+
+import org.junit.Test;
+
+public class GraphDemo {
+    @Test
+    public void readIfcToGraph() {
+
+    }
+
+    @Test
+    public void processIfcGraph() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
